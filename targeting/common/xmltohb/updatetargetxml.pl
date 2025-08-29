458a81d9ab7515c76714fa775a7cc68d0b47af25
deepakala-k <deepakala.karthikeyan@ibm.com>
2025-08-29 04:38:26 -0500

    Rename command options from "hb" to "plat"
    Update the command-line options to replace the term "hb" with "plat",
    which is a more common and appropriate name. This makes the option
    naming consistent and usable across all contexts.
    
    If there is a targetTypeExtension that does not have a targetType, give out a
    warning
    
    Signed-off-by: Deepa Karthikeyan <deepakala.karthikeyan@ibm.com>

--- targeting/common/xmltohb/updatetargetxml.pl
+++ targeting/common/xmltohb/updatetargetxml.pl
@@ -30,11 +30,11 @@
 #
 # Usage:
 #
-#    updatetargetxml --hb=target_types_hb.xml --common=target_types.xml
+#    updatetargetxml --plat=target_types_bmc.xml --common=target_types.xml
 #
 # Purpose:
 #
-#   This perl script processes the target_types_hb.xml file to find the
+#   This perl script processes the target_types_bmc.xml file to find the
 #   <targetTypeExtension> tags, and update target_types.xml if needed.
 #   The updated target_types.xml is written to the console.
 #
@@ -45,27 +45,27 @@ use Data::Dumper;
 
 $XML::Simple::PREFERRED_PARSER = 'XML::Parser';
 
-my $hb     = "";
+my $plat   = "";
 my $common = "";
 my $usage  = 0;
 use Getopt::Long;
 GetOptions(
-    "hb:s"     => \$hb,
+    "plat:s"   => \$plat,
     "common:s" => \$common,
     "help"     => \$usage,
 );
 
-if ( $usage || ( $hb eq "" ) || ( $common eq "" ) )
+if ( $usage || ( $plat eq "" ) || ( $common eq "" ) )
 {
     display_help();
     exit 0;
 }
 
-open( FH, "<$hb" )
-    || die "ERROR: unable to open $hb\n";
+open( FH, "<$plat" )
+    || die "ERROR: unable to open $plat\n";
 close(FH);
 
-my $generic = XMLin( "$hb", ForceArray => 1 );
+my $generic = XMLin( "$plat", ForceArray => 1 );
 
 open( FH, "<$common" )
     || die "ERROR: unable to open $common\n";
@@ -77,6 +77,18 @@ my @NewAttr;
 foreach my $Extension ( @{ $generic->{targetTypeExtension} } )
 {
     my $id = $Extension->{id}->[0];
+
+    # if there is a targetTypeExtension that does not have a targetType,
+    # it does not throw any error, nor adds those attributes. If it prints a warning,
+    # the error gets into the output file as we run this script
+    # and capture its output into a file
+    if ( !exists $generic1->{targetType}->{$id} )
+    {
+        # TODO: as of not it is unable to find targetType for some of the EKB targets
+        # so the error file may not be empty
+        print STDERR "WARNING: targetTypeExtension $id does not have a targetType to map\n";
+    }
+
     foreach my $attr ( @{ $Extension->{attribute} } )
     {
         my $attribute_id = $attr->{id}->[0];
@@ -144,9 +156,9 @@ sub display_help
 Usage:
 
     $scriptname --help
-    $scriptname --hb=hbfname --hb=commonfname
-        --hb=hbfname
-              hbfname is the complete pathname of the target_types_hb.xml file
+    $scriptname --plat=platfname --common=commonfname
+        --plat=platfname
+              platfname is the complete pathname of the target_types_plat.xml file
         --common=commonfname
               commonfname is the complete pathname of the target_types.xml file
 \n";

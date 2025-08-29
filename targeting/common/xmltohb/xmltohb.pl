ac4718a6951cf96e6da18fafa1a05645c889b741
deepakala-k <deepakala.karthikeyan@ibm.com>
2025-08-29 11:44:21 -0500

    Update AttrIdNameMap to include AFFINITY_PATH and native type attributes
    As required by the new targeting code, AFFINITY_PATH must now be present
    in AttrIdNameMap. This commit also adds the native type attributes to
    ensure the map is complete.
    
    Signed-off-by: Deepa Karthikeyan <deepakala.karthikeyan@ibm.com>

--- targeting/common/xmltohb/xmltohb.pl
+++ targeting/common/xmltohb/xmltohb.pl
@@ -2755,7 +2755,8 @@ sub writeAttrIdNameMap
 
         # Simple or complex types
         if (   ( exists $attribute->{simpleType} )
-            || ( exists $attribute->{complexType} ) )
+            || ( exists $attribute->{complexType} )
+            || ( exists $attribute->{nativeType} ) )
         {
             # This loops through all attributes to add id and name
             # not just enumerated attributes
